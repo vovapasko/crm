657ac1ff32320929d3ff6593efb30f9a661f1430
vovapasko <vovapasko1699@gmail.com>
2020-08-28 15:03:45 +0300

    Provided tests for publications and publications blacklist views

--- crm/tests/test_views/contractor_tests/publications_list_tests/publications_list_test_case.py
+++ crm/tests/test_views/contractor_tests/publications_list_tests/publications_list_test_case.py
@@ -1,12 +1,94 @@
+from rest_framework import status
 from rest_framework.reverse import reverse
 
+from crm.models import Contractor
 from crm.tests.base_test_case import BaseTestCase
+from crm.models.contractor_publications_list import ContractorPublicationsList
 
 
 class PublicationsListTestCase(BaseTestCase):
-    # url = reverse('crm:contractor-comments')
+    url = reverse('crm:contractor-publications')
 
-    # @classmethod
-    # def setUpTestData(cls):
-    #     super().setUpTestData()
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.test_user = cls.get_admin_user()
+        cls.test_contractor = Contractor.objects.first()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_get_publications_list_unauthorised(self):
+        client = self.get_api_client(authenticated=False)
+        self.__test_request_unauthorised(method=client.get)
+
+    def test_post_publications_list_unauthorised(self):
+        client = self.get_api_client(authenticated=False)
+        self.__test_request_unauthorised(method=client.post)
+
+    def test_put_publications_list_unauthorised(self):
+        client = self.get_api_client(authenticated=False)
+        self.__test_request_unauthorised(method=client.put)
+
+    def test_delete_publications_list_unauthorised(self):
+        client = self.get_api_client(authenticated=False)
+        self.__test_request_unauthorised(method=client.delete)
+
+    def __test_request_unauthorised(self, method):
+        self._test_request_method_clients(
+            method=method,
+            url=self.url,
+            response_code=status.HTTP_401_UNAUTHORIZED
+        )
+
+    def test_get_publications_list_authorised(self):
+        client = self.get_api_client(user=self.test_user)
+        response = client.get(
+            path=self.url
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_publications_list_authorised(self):
+        client = self.get_api_client(user=self.test_user)
+        self._test_request_method_clients(
+            method=client.post,
+            url=self.url,
+            data={
+                "contractor": self.test_contractor.id,
+                "publish": "Test Edition"
+            },
+            response_code=status.HTTP_201_CREATED
+        )
+
+    def test_put_publications_list_authorised(self):
+        client = self.get_api_client(user=self.test_user)
+        self._test_request_method_clients(
+            method=client.put,
+            url=self.__get_put_delete_url(),
+            response_code=status.HTTP_200_OK,
+            data={"publish": "New Edition"}
+        )
+
+    def test_delete_publications_list_authorised(self):
+        client = self.get_api_client(user=self.test_user)
+        self._test_request_method_clients(
+            method=client.delete,
+            url=self.__get_put_delete_url(),
+            response_code=status.HTTP_204_NO_CONTENT
+        )
+
+    def test_incorrect_post_publications_list_authorised(self):
+        client = self.get_api_client(user=self.test_user)
+        self._test_request_method_clients(
+            method=client.post,
+            url=self.url,
+            data={
+                "contractor": self.test_contractor,  # have to be pk, but here goes entity
+                "publish": "Test comment"
+            },
+            response_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def __get_put_delete_url(self):
+        return self.url + str(ContractorPublicationsList.objects.last().id)
