7829a9b328e585f8fd0f759fb7541f32cd00164e
vovapasko <vovapasko1699@gmail.com>
2020-09-24 11:43:11 +0300

    Provided swagger doc for GmailCredentialsClearView

--- email_app/views/gmail_credentials_clear_view.py
+++ email_app/views/gmail_credentials_clear_view.py
@@ -1,8 +1,33 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.serializers import Serializer
+
 from crm.views.base_view import BaseView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 
 class GmailCredentialsClearView(BaseView):
+    '''
+    Clear token for specified email. Credentials will be reased from database, but Gmail Application
+    still has access to user account. New authentication will be required.
+    '''
+    email_key = 'email'
+
+    # for swagger
+    email_parameter = openapi.Parameter(email_key, openapi.IN_QUERY,
+                                        description="Email credentials of which have to be deleted",
+                                        type=openapi.TYPE_STRING)
+    success_response = openapi.Response('Credentials were cleared successfully')
+    failed_response = openapi.Response('Ty pidor')
+    not_found_response = openapi.Response('Requested email does not exist')
+
+    @swagger_auto_schema(manual_parameters=[email_parameter],
+                         responses={
+                             status.HTTP_200_OK: success_response,
+                             status.HTTP_400_BAD_REQUEST: failed_response,
+                             status.HTTP_404_NOT_FOUND: not_found_response
+                         })
     def post(self, request: Request, *args, **kwargs) -> Response:
         return self.make_response(data={"Message": "Credentials cleared"})
--- email_app/views/gmail_token_revoke_view.py
+++ email_app/views/gmail_token_revoke_view.py
@@ -5,5 +5,10 @@ from crm.views.base_view import BaseView
 
 
 class GmailTokenRevokeView(BaseView):
+    '''
+    Revoke token for specified email. Gmail Application now doesn't have access to user account
+    and new authentication will be required.
+    '''
+
     def post(self, request: Request, *args, **kwargs) -> Response:
         return self.make_response(data={"Message": "Tokens are revoked"})
