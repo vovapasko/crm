8f3efc92230c41b48e429be2c8e95788218cf619
vovapasko <vovapasko1699@gmail.com>
2020-09-25 17:30:37 +0300

    Edited finish_authorize function, removed saving credentials

--- email_app/library/gmail_utils.py
+++ email_app/library/gmail_utils.py
@@ -68,11 +68,7 @@ def finish_authorize(state: str, request_url: str):
     authorization_response = request_url
     flow.fetch_token(authorization_response=authorization_response)
 
-    # Store credentials in the session.
-    # ACTION ITEM: In a production app, you likely want to save these
-    #              credentials in a persistent database instead.
     credentials = flow.credentials
-    save_credentials()
     return credentials_to_dict(credentials)
 
 
