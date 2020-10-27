ed4def6c3c0402be65f21b9990cb66acba09256b
vovapasko <vovapasko1699@gmail.com>
2020-10-27 14:09:40 +0200

    Provided news_wave

--- crm/views/news/news_burst/news_wave_view.py
+++ crm/views/news/news_burst/news_wave_view.py
@@ -7,11 +7,12 @@ from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.request import Request
 from rest_framework.response import Response
 from crm.serializers import NewsWaveSerializer
-from crm.models import NewsWave
+from crm.models import NewsWave, News, WaveFormation, NewsEmail
 from crm.views.base_view import BaseView
 from crm.serializers import NewsWaveCreateSerializer
 from rest_framework import generics
 from crm.paginations import StandardResultsSetPagination
+from typing import List
 
 
 class NewsWaveView(BaseView, generics.ListCreateAPIView, DestroyAPIView, UpdateAPIView):
@@ -62,7 +63,39 @@ class NewsWaveView(BaseView, generics.ListCreateAPIView, DestroyAPIView, UpdateA
                 print(e)
                 return self.json_success_response(response_code=status.HTTP_201_CREATED,
                                                   message={'error': 'error while sending email'})
+            finally:
+                return self.json_success_response(response_code=status.HTTP_201_CREATED,
+                                                  message={'status': 'wave is saved'})
+            # return self.json_success_response(response_code=status.HTTP_201_CREATED,
+            #                                   message={'status': 'wave is saved'})
         return self.json_failed_response(data=serializer.errors)
 
-    def __send_waves_via_email(self, news_project: NewsWave):
+    def __send_waves_via_email(self, news_wave: NewsWave):
+        wave_formation = news_wave.wave_formation
+        news_in_project = news_wave.news_in_project
+        if wave_formation is None:
+            self.__send_news(news_in_project.all())
+        if news_in_project is None:
+            self.__send_wave(wave_formation.all())
+        else:
+            raise Exception("Wave formation and News in Project are empty")
+
+    def __send_news(self, news_in_project: List[News]):
+        for news in news_in_project:
+            email = news.email
+            try:
+                self.__check_credentials(email)
+            except Exception:
+                continue
+            content = news.content
+            contractors = news.contractors.values_list('email', flat=True)
+            title = news.title
+            attachments = news.newsattachment_set.all()
+            print()
+
+    def __send_wave(self, wave_formation: WaveFormation):
         pass
+
+    def __check_credentials(self, email: NewsEmail):
+        if email.gmail_credentials is None:
+            raise Exception("Credentials are missed. Sending is impossible")
