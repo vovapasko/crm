c4400df92650557e736185b55c9e4004397a0ce3
vovapasko <vovapasko1699@gmail.com>
2020-11-02 17:11:07 +0200

    Provided changing in WaveFormationAttachments

--- crm/models/burst_news/wave_formation_attachment.py
+++ crm/models/burst_news/wave_formation_attachment.py
@@ -5,12 +5,18 @@ from ...library.constants.media import wave_formation_attachments_storage
 
 
 class WaveFormationAttachment(AbstractBaseModel):
-    wave_formation = models.ForeignKey('WaveFormation', on_delete=models.CASCADE)
-    file = models.FileField(upload_to=wave_formation_attachments_storage)
+    max_char_field_length = 200
 
-    def delete(self, using=None, keep_parents=False):
-        self.file.delete()  # delete file instance on S3 Bucket
-        super().delete(using=None, keep_parents=False)
+    wave_formation = models.ForeignKey('WaveFormation', on_delete=models.CASCADE)
+    name = models.CharField(max_length=max_char_field_length)
+    base_64 = models.TextField()
+    type = models.CharField(max_length=max_char_field_length)
+
+    # file = models.FileField(upload_to=wave_formation_attachments_storage)
+    #
+    # def delete(self, using=None, keep_parents=False):
+    #     self.file.delete()  # delete file instance on S3 Bucket
+    #     super().delete(using=None, keep_parents=False)
 
     def __str__(self):
-        return self.file.name
+        return self.name
