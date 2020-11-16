6881de0fbf8e633103e17ffd5743e03fa850825a
vovapasko <vovapasko1699@gmail.com>
2020-11-16 08:49:23 +0100

    Added swagger doc

--- email_app/views/gmail_api/gmail_attachments_view.py
+++ email_app/views/gmail_api/gmail_attachments_view.py
@@ -1,3 +1,6 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 from crm.models import NewsEmail
 from crm.views.base_view import BaseView
 from email_app.serializers.gmail_attachment_serializer import GmailAttachmentSerializer
@@ -11,6 +14,20 @@ class GmailAttachmentsView(BaseView):
     message_param = 'message_id'
     attachment_param = 'attachment_id'
 
+    email_parameter = openapi.Parameter(email_param,
+                                        openapi.IN_QUERY,
+                                        description="Email for inbox",
+                                        type=openapi.TYPE_STRING)
+    message_parameter = openapi.Parameter(message_param, openapi.IN_QUERY,
+                                          description="The ID of the message containing the attachment",
+                                          type=openapi.TYPE_STRING)
+    attachment_parameter = openapi.Parameter(attachment_param, openapi.IN_QUERY,
+                                             description="The ID of the attachment",
+                                             type=openapi.TYPE_STRING)
+    response = openapi.Response('Gets the specified message attachment with format {size: int, data: base64_str}')
+
+    @swagger_auto_schema(manual_parameters=[email_parameter, message_parameter, attachment_parameter],
+                         responses={200: response})
     def get(self, request, *args, **kwargs):
         serializer = self.serializer(data=request.GET)
         if serializer.is_valid():
