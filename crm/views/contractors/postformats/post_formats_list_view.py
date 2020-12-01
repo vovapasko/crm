4751c6fa8d87be1dc6df744ff52907f292efcc81
vovapasko <vovapasko1699@gmail.com>
2020-12-01 21:29:13 +0100

    Provided swagger doc for PostFormatsListView

--- crm/views/contractors/postformats/post_formats_list_view.py
+++ crm/views/contractors/postformats/post_formats_list_view.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions, status
 from rest_framework.generics import UpdateAPIView, ListCreateAPIView, DestroyAPIView
 from crm.permissions import DjangoModelNoGetPermissions
@@ -16,16 +18,37 @@ class PostFormatListView(BaseView, ListCreateAPIView, UpdateAPIView, DestroyAPIV
     pagination_class = StandardResultsSetPagination
     queryset = PostFormatList.objects.all().order_by('id')
 
+    contractor_param = 'contractor'
+    post_format_list_param = 'pk'
+
+    contractor_swagger_param = openapi.Parameter(contractor_param, openapi.IN_QUERY,
+                                                 description="get post formats for concrete contractor",
+                                                 type=openapi.TYPE_INTEGER)
+    post_format_list_swagger_param = openapi.Parameter(post_format_list_param, openapi.IN_QUERY,
+                                                       description='Get concrete post format',
+                                                       type=openapi.TYPE_INTEGER)
+    post_format_list_swagger_get_response = openapi.Response(description='Get Post Format list',
+                                                             schema=serializer_class)
+
+    @swagger_auto_schema(manual_parameters=[contractor_swagger_param, post_format_list_swagger_param],
+                         responses={200: post_format_list_swagger_get_response})
     def get(self, request, *args, **kwargs):
-        contractor_id = kwargs.get('contractor', None)
-        if contractor_id:
+        contractor_id = kwargs.get(self.contractor_param, None)
+        postformat_id = kwargs.get(self.post_format_list_param, None)
+        if contractor_id and postformat_id:
+            queryset = self.queryset.filter(id=postformat_id)
+            return self.make_response(
+                data=self.serializer_class(
+                    instance=queryset, many=True
+                ).data
+            )
+        else:
             queryset = self.queryset.filter(contractor=contractor_id)
             return self.make_response(
                 data=self.serializer_class(
                     instance=queryset, many=True
                 ).data
             )
-        return super(PostFormatListView, self).get(request, *args, **kwargs)
 
     def delete(self, request: Request, *args, **kwargs) -> Response:
         return super().delete(request, *args, **kwargs)
