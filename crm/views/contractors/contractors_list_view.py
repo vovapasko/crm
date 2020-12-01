702d3b2bd4a80bad53a67cd0550d57adbf9a45c0
vovapasko <vovapasko1699@gmail.com>
2020-12-01 20:21:54 +0100

    Edited ContractorsListView, added templates for delete method and removed boilerplate code in post method

--- crm/views/contractors/contractors_list_view.py
+++ crm/views/contractors/contractors_list_view.py
@@ -1,9 +1,8 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import UpdateAPIView, DestroyAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-
 from ...library.constants import MESSAGE_JSON_KEY
 from ...paginations import StandardResultsSetPagination
 from crm.serializers import ContractorSerializer
@@ -13,45 +12,37 @@ from ..base_view import BaseView
 from ...permissions import DjangoModelNoGetPermissions
 
 
-class ContractorsListView(BaseView, generics.ListCreateAPIView, UpdateAPIView):
+class ContractorsListView(BaseView, generics.ListCreateAPIView, UpdateAPIView, DestroyAPIView):
     queryset = Contractor.objects.all().order_by('id')
     permission_classes = [permissions.IsAuthenticated, DjangoModelNoGetPermissions]
     serializer_class = ContractorSerializer
     pagination_class = StandardResultsSetPagination
 
     def post(self, request: Request, *args, **kwargs) -> Response:
-        """
-        Creates new contractor
-        body params:
-            - editor_name: str
-            - contact_person: str
-            - phone_number: str
-            - email: str (email)
-            - news_amount: positive int
-            - arranged_news: positive int
-            - one_post_price: positive int
-        :return json response
-            http response codes:
-                200 - ok, contractor was created successfully
-                400 - validation error or bad request
-                401 - bad JWT token or user unauthorized
-            keys:
-                response_code - response code
-                errors - errors during validation
-                data - all created contractor's data
-                message - success message about successful creating
-                contractor
-                detail - message if user was not authorised or had bad JWT token
-        """
-        serializer = self.serializer_class(data=request.data)
-
-        if serializer.is_valid():
-            return self.__save_contractor_send_response(
-                serializer,
-                f"Created contractor",
-            )
-
-        return self.json_failed_response(errors=serializer.errors)
+        #     """
+        #     Creates new contractor
+        #     body params:
+        #         - editor_name: str
+        #         - contact_person: str
+        #         - phone_number: str
+        #         - email: str (email)
+        #         - news_amount: positive int
+        #         - arranged_news: positive int
+        #         - one_post_price: positive int
+        #     :return json response
+        #         http response codes:
+        #             200 - ok, contractor was created successfully
+        #             400 - validation error or bad request
+        #             401 - bad JWT token or user unauthorized
+        #         keys:
+        #             response_code - response code
+        #             errors - errors during validation
+        #             data - all created contractor's data
+        #             message - success message about successful creating
+        #             contractor
+        #             detail - message if user was not authorised or had bad JWT token
+        #     """
+        return super().post(request, *args, **kwargs)
 
     def put(self, request: Request, *args, **kwargs) -> Response:
         return self.partial_update(request)
@@ -70,20 +61,7 @@ class ContractorsListView(BaseView, generics.ListCreateAPIView, UpdateAPIView):
                 errors - errors during validation
                 message - success message about successful deleting contractor
         """
-        try:
-            contractor = get_object_or_404(Contractor, pk=pk)
-        except Http404:
-            return self.json_failed_response(
-                response_code=status.HTTP_404_NOT_FOUND,
-                errors=dict(
-                    error=f"Contractor with id {pk} does not exist"
-                )
-            )
-
-        contractor.delete()
-        return self.json_success_response(
-            message={MESSAGE_JSON_KEY: f"Contractor {pk} was deleted successfully"},
-        )
+        return super().delete(request, *args, **kwargs)
 
     def __save_contractor_send_response(self, serializer: ContractorSerializer, send_message: str) -> Response:
         serializer.save()
