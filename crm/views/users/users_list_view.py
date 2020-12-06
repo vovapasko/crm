8cd93f6bc25a43eefc03073fbfa9de0907c17020
vovapasko <vovapasko1699@gmail.com>
2020-12-06 14:50:37 +0100

    Provided retrieving entity for User

--- crm/views/users/users_list_view.py
+++ crm/views/users/users_list_view.py
@@ -7,14 +7,14 @@ from crm.permissions.can_delete_permission import CanDeletePermission
 from crm.library.constants import MESSAGE_JSON_KEY
 from crm.paginations import StandardResultsSetPagination
 from crm.serializers import UserSerializer
-from rest_framework.generics import ListCreateAPIView, UpdateAPIView
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from crm.models import User
 from crm.views.base_view import BaseView
 from crm.permissions import DjangoModelNoGetPermissions
 
 
-class UsersListView(BaseView, ListCreateAPIView, UpdateAPIView):
+class UsersListView(BaseView, ListCreateAPIView, UpdateAPIView, RetrieveAPIView):
     queryset = User.objects.all().filter(is_archived=False).order_by('id')
     permission_classes = [IsAuthenticated, CanDeletePermission, DjangoModelNoGetPermissions]
     serializer_class = UserSerializer
@@ -45,3 +45,8 @@ class UsersListView(BaseView, ListCreateAPIView, UpdateAPIView):
 
     def put(self, request, *args, **kwargs):
         return super().partial_update(request)
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get('pk'):
+            return super().retrieve(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
