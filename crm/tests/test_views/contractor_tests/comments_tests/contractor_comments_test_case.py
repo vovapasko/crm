a2ed12b29c30cb6647957c64d777e78ac1dcd486
sa1exx <sandrez.spartanell@gmail.com>
2020-12-22 12:11:24 +0200

    replaced by reusable field

--- crm/tests/test_views/contractor_tests/comments_tests/contractor_comments_test_case.py
+++ crm/tests/test_views/contractor_tests/comments_tests/contractor_comments_test_case.py
@@ -11,6 +11,9 @@ class ContractorCommentsTestCase(BaseTestCase):
         super().setUpTestData()
         cls.test_user = cls.get_admin_user()
         cls.test_contractor = Contractor.objects.first()
+        cls.post_get_url = reverse('crm:contractor-comments', kwargs={'contractor': cls.test_contractor.id})
+        cls.put_delete_url = reverse('crm:contractor-comments', kwargs={'contractor': cls.test_contractor.id,
+                                                                    'pk': str(ContractorCommentList.objects.last().id)})
 
     @classmethod
     def tearDownClass(cls):
@@ -33,28 +36,24 @@ class ContractorCommentsTestCase(BaseTestCase):
         self.__test_request_unauthorised(method=client.delete)
 
     def __test_request_unauthorised(self, method):
-        url = self.__comments_url(contractor=self.test_contractor.id)
-
         self._test_request_method_clients(
             method=method,
-            url=url,
+            url=self.post_get_url,
             response_code=status.HTTP_401_UNAUTHORIZED
         )
 
     def test_get_comment_authorised(self):
         client = self.get_api_client(user=self.test_user)
-        url = self.__comments_url(contractor=self.test_contractor.id)
         response = client.get(
-            path=url
+            path=self.post_get_url
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_comment_authorised(self):
         client = self.get_api_client(user=self.test_user)
-        url = self.__comments_url(contractor=self.test_contractor.id)
         self._test_request_method_clients(
             method=client.post,
-            url=url,
+            url=self.post_get_url,
             data={
                 "contractor": self.test_contractor.id,
                 "comment": "Test comment"
@@ -66,26 +65,24 @@ class ContractorCommentsTestCase(BaseTestCase):
         client = self.get_api_client(user=self.test_user)
         self._test_request_method_clients(
             method=client.put,
-            url=self.__get_put_delete_url(),
+            url=self.put_delete_url,
             response_code=status.HTTP_200_OK,
             data={"comment": "Edited comment"}
         )
 
     def test_delete_comment_authorised(self):
         client = self.get_api_client(user=self.test_user)
-        url = self.__get_put_delete_url()
         self._test_request_method_clients(
             method=client.delete,
-            url=url,
+            url=self.put_delete_url,
             response_code=status.HTTP_204_NO_CONTENT
         )
 
     def test_incorrect_post_comment_authorised(self):
         client = self.get_api_client(user=self.test_user)
-        url = self.__comments_url(contractor=self.test_contractor.id)
         self._test_request_method_clients(
             method=client.post,
-            url=url,
+            url=self.post_get_url,
             data={
                 "contractor": self.test_contractor,  # have to be pk, but here goes entity
                 "comment": "Test comment"
